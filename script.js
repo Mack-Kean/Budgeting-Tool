e27223abf9c358f694b570552abf1c0ec9470df5
Mack-Kean <36168905+Mack-Kean@users.noreply.github.com>
2020-09-01 11:21:41 -0400

    added chart functionality

--- script.js
+++ script.js
@@ -4,9 +4,12 @@ var expenseField = document.getElementById('expense_name');
 var dollarField = document.getElementById('dollar_amount');
 var errorText = document.getElementById('error_text');
 
+var totalBudget = document.getElementById('total');
+var totalbudgetInt = 0;
+
 addButton.onclick = function() {
 
-    errorText = ''; //resets error text when new input is entered
+    errorText.textContent = ''; //resets error text when new input is entered
 
     if (parseInt(dollarField.value) < 0){
         //allows negative numbers to be ignored
@@ -20,18 +23,26 @@ addButton.onclick = function() {
         if (dollarField.value.length == 0){
             amount = document.createTextNode('0');
         } else {
+            //case for when an actual positive number is entered
             amount = document.createTextNode(dollarField.value);
+            totalbudgetInt += parseInt(dollarField.value) //adds to total
         }
         textCell.appendChild(text);
         numCell.appendChild(amount);
+        drawChart();
+        totalBudget.innerHTML = "$" + totalbudgetInt;
     }
+
+    //empty text and dollar fields
+    dollarField.value = ''; 
+    expenseField.value = '';
 
 }
 
 // Load the Visualization API and the corechart package.
 google.charts.load('current', {'packages':['corechart']});
 
-function readyForChart(){
+function readyForChart() {
     // Set a callback to run when the Google Visualization API is loaded.
     google.charts.setOnLoadCallback(drawChart);
 }
@@ -45,13 +56,14 @@ function drawChart() {
   var data = new google.visualization.DataTable();
   data.addColumn('string', 'Expenses');
   data.addColumn('number', 'Budget');
- /* data.addRows([
-    ['Mushrooms', 3],
-    ['Onions', 1],
-    ['Olives', 1],
-    ['Zucchini', 1],
-    ['Pepperoni', 2]
-  ]); */
+
+  var tableRows = tableBody.querySelectorAll('tr');
+
+  for (var i = 0; i < tableRows.length; i++){
+    data.addRows([
+    [tableRows[i].cells[0].textContent, parseInt(tableRows[i].cells[1].textContent)],
+    ]); 
+  }
 
   // Set chart options
   var options = {'title':'Budget Breakdown',
